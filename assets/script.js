02f09b79893d19602ac3d6cff30ecaa1f603371e
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-09-10 21:22:00 -0500

    made changes to javascript

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var containerEl = document.querySelector('.container');
+var currentdayEl = document.querySelector('.lead')
 
 for (var i = 0; i < 9; i++){
     var rowEl = document.createElement('div');
@@ -6,16 +7,21 @@ var hourEl = document.createElement('div');
 var textareaEl = document.createElement('textarea');
 var buttonEl = document.createElement('button');
 
-
 rowEl.setAttribute('class', 'row time-block');
 hourEl.setAttribute('class', 'hour col-md-1');
-textareaEl.setAttribute('class', 'past description col-md-10');
+textareaEl.setAttribute('class', 'description col-md-10');
 buttonEl.setAttribute('class', 'saveBtn col-md-1');
 
 rowEl.appendChild(hourEl);
 rowEl.appendChild(textareaEl);
 rowEl.appendChild(buttonEl);
-containerEl.append(rowEl);
+containerEl.appendChild(rowEl);
+
+buttonEl.textContent = "Click to Save";
 
 hourEl.textContent = moment().hour(i + 9).format("hA");
 }
+
+var Date = moment().format("dddd MMM Do");
+$("#currentDay").text(Date);
+
